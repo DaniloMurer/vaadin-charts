38ce6e2bca8ee5664276be808e3208430296085a
Jakob Danilo <danilo.jakob@wuerth-itensis.com>
2022-02-07 14:09:00 +0100

    CHANGE: Created TimeLine chart, or better mimicked a timeline chart, because vaadin 14 doesn't have one

---
+++ frontend/generated/theme.js
@@ -0,0 +1,2 @@
+import {applyTheme as _applyTheme} from 'generated/theme-vaadin-charts.generated.js';
+export const applyTheme = _applyTheme;
--- src/main/java/com/danilojakob/vaadin/charts/views/helloworld/HelloWorldView.java
+++ src/main/java/com/danilojakob/vaadin/charts/views/helloworld/HelloWorldView.java
@@ -2,8 +2,11 @@ package com.danilojakob.vaadin.charts.views.helloworld;
 
 import com.danilojakob.vaadin.charts.views.MainLayout;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.charts.Chart;
+import com.vaadin.flow.component.charts.model.*;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
@@ -12,22 +15,42 @@ import com.vaadin.flow.router.RouteAlias;
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
-public class HelloWorldView extends HorizontalLayout {
-
-    private TextField name;
-    private Button sayHello;
+public class HelloWorldView extends VerticalLayout {
 
     public HelloWorldView() {
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
+        setSpacing(true);
 
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
 
-        add(name, sayHello);
+        add(createTimeLineChart());
+    }
+
+    private Chart createTimeLineChart() {
+        Chart timelineChart = new Chart(ChartType.LINE);
+        Configuration timelineChartConfiguration = timelineChart.getConfiguration();
+        timelineChartConfiguration.setTitle("Timeline Chart");
+
+
+        XAxis xAxis = new XAxis();
+        xAxis.setCategories("01.01.2022", "02.01.2022", "03.01.2022", "04.01.2022", "05.01.2022", "06.01.2022");
+        timelineChartConfiguration.addxAxis(xAxis);
+        timelineChartConfiguration.getyAxis().setVisible(false);
+
+        ListSeries listSeries = new ListSeries("Timeline", 0, 0, 0, 0, 0, 0);
+        listSeries.setId("timeline");
+        timelineChartConfiguration.addSeries(listSeries);
+        timelineChartConfiguration.getTooltip().setEnabled(true);
+
+        DataSeries flagSeries = new DataSeries("Flag Series");
+        PlotOptionsFlags plotOptionsFlags = new PlotOptionsFlags();
+        plotOptionsFlags.setOnSeries("timeline");
+        plotOptionsFlags.setShape(FlagShape.CALLOUT);
+        flagSeries.setPlotOptions(plotOptionsFlags);
+        flagSeries.add(new FlagItem(1, "02.01.22 - First Step Done"));
+        flagSeries.add(new FlagItem(4, "05.01.22 - Second Step Done"));
+        flagSeries.add(new FlagItem(5, "06.01.22 - Third Step Done"));
+
+        timelineChartConfiguration.addSeries(flagSeries);
+        return timelineChart;
     }
 
 }
